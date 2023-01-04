6da53b5a76176141917b27d02f20172a5e2e62b0
AlexNikola23 <qwert1967@bk.ru>
2023-01-04 19:09:31 +0300

    типы данных и переменные

--- less2.py
+++ less2.py
@@ -1,11 +1,10 @@
-1 # int
-1.564 #float
-"figa5" #str
-True #bool
-False #bool
-None # пустой тип
 per1="help"
+per6="factory"
 per2=234
+per7=87
 per3=526.32
 per4=None
 per5=True
+per8=45.47
+per9=None
+per10=False
